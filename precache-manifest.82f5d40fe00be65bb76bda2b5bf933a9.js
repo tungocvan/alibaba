70571a88ae6893866dbba577162d9c4eab5726ee
tungocvan <tungocvan@gmail.com>
2019-09-11 11:19:09 +0700

    isUpdate3

---
+++ precache-manifest.82f5d40fe00be65bb76bda2b5bf933a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1c3a5612a381ad73ace9da18372d38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0abfd065bf91bb11bf4",
+    "url": "/static/css/main.cf6db5bd.chunk.css"
+  },
+  {
+    "revision": "8f1aae09ab15b9f6f7d2",
+    "url": "/static/js/2.beee8ac7.chunk.js"
+  },
+  {
+    "revision": "e0abfd065bf91bb11bf4",
+    "url": "/static/js/main.94cc4451.chunk.js"
+  },
+  {
+    "revision": "835cae36aaa8e6a9c8db",
+    "url": "/static/js/runtime~main.5b93e505.js"
+  }
+]);
